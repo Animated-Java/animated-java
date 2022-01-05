fc789b62957b22ed73a2867d339f73ae25027e3e
Ian Senne (FetchBot) <IanSSenne@gmail.com>
2022-01-05 12:09:08 -0800

    test

---
+++ tools/distribute.js
@@ -0,0 +1,24 @@
+const { initializeApp } = require('firebase/app')
+const { getStorage, ref, uploadString } = require('firebase/storage')
+const { getInput } = require('@actions/core')
+const app = initializeApp({
+	apiKey: getInput('API_KEY'),
+	authDomain: getInput('AUTH_DOMAIN'),
+	projectId: getInput('PROJECT_ID'),
+	storageBucket: getInput('STORAGE_BUCKET'),
+	messagingSenderId: getInput('MESSAGE_SENDER_ID'),
+	appId: getInput('APP_ID'),
+	measurementId: getInput('MEASUREMENT_ID'),
+})
+const store = getStorage(app)
+const build = ref(store, `builds/${getInput('GITHUB_REF')}/animated_java.js`)
+
+const fs = require('fs')
+uploadString(build, fs.readFileSync('./dist/animated_java.js', 'utf8')).then(
+	(snapshot) => {
+		console.log(
+			'Uploaded animated java to ',
+			`builds/${getInput('GITHUB_REF')}/animated_java.js`
+		)
+	}
+)
