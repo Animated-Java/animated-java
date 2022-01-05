3d9069931e89c8457eec715fe33f78d5fcd656ff
Ian Senne (FetchBot) <IanSSenne@gmail.com>
2022-01-05 12:58:46 -0800

    test3

--- tools/distribute.js
+++ tools/distribute.js
@@ -3,19 +3,16 @@ const { getStorage, ref, uploadString } = require('firebase/storage')
 const core = require('@actions/core')
 console.log(Object.keys(process.env))
 const app = initializeApp({
-	apiKey: core.getInput('API_KEY'),
-	authDomain: core.getInput('AUTH_DOMAIN'),
-	projectId: core.getInput('PROJECT_ID'),
-	storageBucket: core.getInput('STORAGE_BUCKET'),
-	messagingSenderId: core.getInput('MESSAGE_SENDER_ID'),
-	appId: core.getInput('APP_ID'),
-	measurementId: core.getInput('MEASUREMENT_ID'),
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGE_SENDER_ID,
+	appId: process.env.APP_ID,
+	measurementId: process.env.MEASUREMENT_ID,
 })
 const store = getStorage(app)
-const build = ref(
-	store,
-	`builds/${core.getInput('GITHUB_REF')}/animated_java.js`
-)
+const build = ref(store, `builds/${process.env.GITHUB_REF}/animated_java.js`)
 
 const fs = require('fs')
 uploadString(build, fs.readFileSync('./dist/animated_java.js', 'utf8')).then(
