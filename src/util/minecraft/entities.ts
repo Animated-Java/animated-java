d7659744a5de4db7ef0da0b5dbc91056d25ea950
SnaveSutit <snavesutit@gmail.com>
2022-02-09 16:14:17 -0500

    Updated item and entity registries to use Misode's git repo
    Also added caching so we don't pull the data every time you start Blockbench

--- src/util/minecraft/entities.ts
+++ src/util/minecraft/entities.ts
@@ -1,74 +1,13 @@
-import { CustomError } from '../customError'
+import { registry } from './registryLoader'
 
 export class Entities {
-	static entities = []
+	static list = []
 
 	static isEntity(name: string) {
-		return Entities.entities.includes(name)
+		return Entities.list.includes(name)
 	}
 }
 
-const gotten = localStorage.getItem('animated-java<minecraftEntityList>')
-
-let lsItem = { values: [] }
-try {
-	lsItem = JSON.parse(gotten)
-	Entities.entities = lsItem.values
-} catch (e) {
-	lsItem = { values: [] }
-}
-
-const maxFails = 2
-let fails = 0
-
-function queryEntities() {
-	function end() {
-		throw new CustomError(
-			'Unable to fetch minecraft item list, and item list not defined in local storage.'
-		)
-	}
-
-	fetch(
-		'https://raw.githubusercontent.com/Arcensoth/mcdata/master/processed/reports/registries/entity_type/data.json'
-	)
-		.catch((e) => {
-			console.error(e)
-		})
-		.then(
-			async (response) => {
-				if (response) {
-					const json = await response.json()
-					if (json) {
-						console.log(
-							'Successfully Collected Entity List:',
-							json.values
-						)
-						Entities.entities = json.values
-						localStorage.setItem(
-							'animated-java<minecraftEntityList>',
-							JSON.stringify(json)
-						)
-						return
-					}
-				}
-				fails += 1
-				if (fails <= maxFails) {
-					setTimeout(queryEntities, 1000)
-				} else if (!lsItem.values.length) {
-					end()
-				}
-			},
-			(e) => {
-				fails += 1
-				if (fails <= maxFails) {
-					setTimeout(queryEntities, 1000)
-				} else if (lsItem.values.length) {
-					Entities.entities = lsItem.values
-				} else {
-					end()
-				}
-			}
-		)
-}
-
-queryEntities()
+registry.then(v => {
+	Entities.list = v.entity_type.map(v => `minecraft:${v}`)
+})
