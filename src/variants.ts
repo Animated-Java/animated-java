3ab0d415b08330755074997cb475a4518755ec58
SnaveSutit <snavesutit@gmail.com>
2022-04-19 15:42:10 -0400

    Variant updates and more BlockBench Typing 🤮
    - Added validation function to Variant class
    - Added uuid property to the Texture class type

--- src/variants.ts
+++ src/variants.ts
@@ -10,10 +10,23 @@ class Variant {
 		this.boneConfigs = {}
 	}
 
+	validate() {
+		const missingTextures = []
+		const missingBones = []
+		for (const [origin, replacer] of Object.entries(this.textureMap)) {
+			if (!Texture.all.find(v => v.uuid == origin)) missingTextures.push(origin)
+			if (!Texture.all.find(v => v.uuid == replacer)) missingTextures.push(replacer)
+		}
+		for (const [bone, _] of Object.entries(this.boneConfigs)) {
+			if (!Group.all.find(v => v.uuid == bone)) missingBones.push(bone)
+		}
+		if (missingBones.length && missingTextures.length) return { missingBones, missingTextures }
+	}
+
 	toJSON() {
 		return {
 			textureMap: this.textureMap,
-			boneConfigs: this.boneConfigs
+			boneConfigs: this.boneConfigs,
 		}
 	}
 
