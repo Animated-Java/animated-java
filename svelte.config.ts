5f4ae05dab01f24e1b1b8c30e7b10fe2064df020
Ian Senne <ian@iansenne.com>
2023-02-22 04:11:23 -0800

    have events delete css

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,5 +1,7 @@
+import { resolve } from 'path'
 import prep from 'svelte-preprocess'
 import { typescript } from 'svelte-preprocess-esbuild'
+
 export const compilerOptions = {
 	dev: process.env.NODE_ENV === 'development',
 	css: true,
@@ -14,9 +16,13 @@ export const preprocess = [
 	}),
 	prep({ typescript: false }),
 ]
+
+const importPath = resolve(__dirname, '../src', 'events.ts')
+
 export const transformCssToJs = (css: string) => {
-	return `css:{const $deletable = Blockbench.addCSS(${JSON.stringify(
-		css
-	)});console.log($deletable);}`
+	return `import {events} from ${JSON.stringify(importPath)};
+	const $deletable = Blockbench.addCSS(${JSON.stringify(css)});
+	events.unload.addListener(() => $deletable());
+	events.uninstall.addListener(() => $deletable());`
 }
 export default { preprocess, transformCssToJs }
