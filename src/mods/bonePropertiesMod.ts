5482374bb16ef00e820ba92b0ddd3fd596ee43ad
SnaveSutit <snavesutit@gmail.com>
2024-08-01 14:41:38 -0400

    🛠️ Improved issue in #222

--- src/mods/bonePropertiesMod.ts
+++ src/mods/bonePropertiesMod.ts
@@ -3,13 +3,29 @@ import { PACKAGE } from '../constants'
 import { roundToNth } from '../util/misc'
 import { type ContextProperty, createBlockbenchMod } from '../util/moddingTools'
 
+class DeepClonedObjectProperty extends Property<'object'> {
+	constructor(targetClass: any, name: string, options?: PropertyOptions) {
+		super(targetClass, 'object', name, options)
+	}
+	merge(instance: any, data: any) {
+		if (typeof data[this.name] === 'object') {
+			instance[this.name] = JSON.parse(JSON.stringify(data[this.name]))
+		}
+	}
+	copy(instance: any, target: any) {
+		if (typeof instance[this.name] === 'object') {
+			target[this.name] = JSON.parse(JSON.stringify(instance[this.name]))
+		}
+	}
+}
+
 createBlockbenchMod(
 	`${PACKAGE.name}:boneProperties`,
 	{
-		configs: undefined as ContextProperty<'instance'>,
+		configs: undefined as ContextProperty<'object'>,
 	},
 	context => {
-		context.configs = new Property(Group, 'instance', 'configs', {
+		context.configs = new DeepClonedObjectProperty(Group, 'configs', {
 			condition,
 			default: { default: undefined, variants: {} },
 		})
