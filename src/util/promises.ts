4ac11bb25f2f746c36ee7c20adabd96aab3acc49
SnaveSutit <snavesutit@gmail.com>
2025-10-11 14:13:51 -0400

    🎨 Improve `pollUntilResult`

--- src/util/promises.ts
+++ src/util/promises.ts
@@ -1,24 +1,48 @@
+export class PollingCancelledError extends Error {
+	constructor() {
+		super('Polling was cancelled')
+		this.name = 'PollingCancelledError'
+	}
+}
+
 /**
- * Polls {@link fn} until it returns a non-nullish value, then resolves the promise with that value.
- *
- * if {@link cancelCondition} returns true while polling, the promise is rejected.
+ * Polls a function every frame until it returns a non-nullish value, or until a cancel condition is met or a timeout occurs.
  */
 export function pollUntilResult<T>(
-	fn: () => T | undefined | null,
+	/**
+	 * The function to poll. Should return a non-nullish value to resolve the promise.
+	 */
+	callback: () => T | undefined | null,
+	/**
+	 * If this condition returns true, polling will be cancelled.
+	 */
 	cancelCondition: () => boolean,
-	interval = 100
+	/**
+	 * Maximum time to wait before timing out, in milliseconds. Defaults to 1 minute.
+	 */
+	timeout = 1000 * 60,
+	/**
+	 * Time between polls in milliseconds. Defaults to polling every animation frame.
+	 */
+	interval?: number
 ): Promise<T> {
+	const startTime = performance.now()
+
+	const timeoutCallback =
+		interval !== undefined
+			? (cb: FrameRequestCallback) => setTimeout(cb, interval)
+			: requestAnimationFrame
+
 	return new Promise<T>((resolve, reject) => {
-		const intervalId = setInterval(() => {
-			if (cancelCondition()) {
-				clearInterval(intervalId)
-				return reject('pollUntilResult Cancelled')
-			}
-			const result = fn()
-			if (result != undefined) {
-				clearInterval(intervalId)
-				resolve(result)
-			}
-		}, interval)
+		const poll = () => {
+			if (cancelCondition()) return reject(new PollingCancelledError())
+			const result = callback()
+			if (result != undefined) return resolve(result)
+			if (performance.now() - startTime > timeout)
+				return reject(new Error('Polling timed out'))
+			timeoutCallback(poll)
+		}
+		// poll() // Check immediately in case the condition is already met
+		timeoutCallback(poll)
 	})
 }
