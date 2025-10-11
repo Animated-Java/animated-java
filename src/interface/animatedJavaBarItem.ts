366eb5148d0b012e5a90bb7b10031ea89f83500f
SnaveSutit <snavesutit@gmail.com>
2025-10-11 14:32:52 -0400

    ✨ Add `Export All` actions

--- src/interface/animatedJavaBarItem.ts
+++ src/interface/animatedJavaBarItem.ts
@@ -26,7 +26,8 @@ function createIconImg() {
 	return img
 }
 
-const SEPARATOR = new MenuSeparator('animated-java:menu-separator/menubar-separator')
+const SEPARATOR_A = new MenuSeparator('animated-java:menu-separator/menubar-separator-a')
+const SEPARATOR_B = new MenuSeparator('animated-java:menu-separator/menubar-separator-b')
 
 const OPEN_ABOUT = registerAction(
 	{ id: 'animated-java:action/about' },
@@ -64,6 +65,54 @@ const OPEN_CHANGELOG = registerAction(
 	}
 )
 
+function areMultipleBlueprintsOpen() {
+	return Blockbench.ModelProject.all.filter(p => p.format.id === BLUEPRINT_FORMAT_ID).length > 1
+}
+
+async function exportAll(debugMode: boolean) {
+	const selectedProject = Project
+	const blueprints = Blockbench.ModelProject.all.filter(p => p.format.id === BLUEPRINT_FORMAT_ID)
+	let success = true
+	for (const project of blueprints) {
+		project.select()
+		await new Promise(resolve => requestAnimationFrame(resolve))
+		success = await exportProject({ debugMode })
+		if (!success) break
+		await new Promise(resolve => requestAnimationFrame(resolve))
+	}
+	if (success) selectedProject?.select()
+}
+
+const EXPORT_ALL_DEBUG = registerAction(
+	{ id: 'animated-java:action/export-all-debug' },
+	{
+		icon: 'bug_report',
+		category: 'animated_java',
+		name: translate('action.export_all_debug.name'),
+		description: translate('action.export_all_debug.description'),
+		condition: areMultipleBlueprintsOpen,
+		click() {
+			console.log('Exporting all open Blueprints in development mode...')
+			void exportAll(true)
+		},
+	}
+)
+
+const EXPORT_ALL = registerAction(
+	{ id: 'animated-java:action/export-all' },
+	{
+		icon: 'insert_drive_file',
+		category: 'animated_java',
+		name: translate('action.export_all.name'),
+		description: translate('action.export_all.description'),
+		condition: areMultipleBlueprintsOpen,
+		click() {
+			console.log('Exporting all open Blueprints...')
+			void exportAll(false)
+		},
+	}
+)
+
 const OPEN_BLUEPRINT_SETTINGS = registerAction(
 	{ id: 'animated-java:action/blueprint-settings' },
 	{
@@ -90,6 +139,20 @@ const EXTRACT = registerAction(
 	}
 )
 
+const EXPORT_DEBUG = registerAction(
+	{ id: 'animated-java:action/export-debug' },
+	{
+		icon: 'bug_report',
+		category: 'animated_java',
+		name: translate('action.export_debug.name'),
+		condition: activeProjectIsBlueprintFormat,
+		click() {
+			void exportProject({ debugMode: true })
+		},
+		keybind: new Keybind({ ctrl: true, key: 69 /* E */ }),
+	}
+)
+
 const EXPORT = registerAction(
 	{ id: 'animated-java:action/export' },
 	{
@@ -101,20 +164,6 @@ const EXPORT = registerAction(
 			void exportProject()
 		},
 		keybind: new Keybind({ ctrl: true, shift: true, key: 69 /* E */ }),
-	}
-)
-
-const EXPORT_DEV = registerAction(
-	{ id: 'animated-java:action/export-dev' },
-	{
-		icon: 'bug_report',
-		category: 'animated_java',
-		name: translate('action.export_debug.name'),
-		condition: activeProjectIsBlueprintFormat,
-		click() {
-			void exportProject({ debugMode: true })
-		},
-		keybind: new Keybind({ ctrl: true, key: 69 /* E */ }),
 	}
 )
 
@@ -145,10 +194,13 @@ MENUBAR.onCreated(menubar => {
 				OPEN_ABOUT.get(),
 				OPEN_DOCUMENTATION.get(),
 				OPEN_CHANGELOG.get(),
-				SEPARATOR,
+				SEPARATOR_A,
+				EXPORT_ALL_DEBUG.get(),
+				EXPORT_ALL.get(),
+				SEPARATOR_B,
 				OPEN_BLUEPRINT_SETTINGS.get(),
 				createExtractSubMenu(),
-				EXPORT_DEV.get(),
+				EXPORT_DEBUG.get(),
 				EXPORT.get(),
 			]
 
