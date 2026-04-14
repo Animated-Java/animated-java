e8e9f3cabfdc09fac40b5169d9d67536913f12f1
SnaveSutit <SnaveSutit@gmail.com>
2026-04-14 11:53:18 -0400

    ⚰️ Remove duplicate `changelogDialog.ts`

--- src/dialogs/changelog/changelog.ts
+++ src/dialogs/changelog/changelog.ts
@@ -3,13 +3,13 @@ import { SvelteDialog } from '../../util/svelteDialog'
 import { translate } from '../../util/translation'
 import ChangelogDialog from './changelog.svelte'
 
-export const DIALOG_ID = `${PACKAGE.name}:animationPropertiesDialog`
+export const DIALOG_ID = `${PACKAGE.name}:changelogDialog`
 
 export function openChangelogDialog() {
 	new SvelteDialog({
 		id: DIALOG_ID,
 		title: translate('dialog.changelog_dialog.title'),
-		width: 600,
+		width: 800,
 		content: { component: ChangelogDialog },
 		buttons: ['OK!'],
 		preventKeybinds: true,
