5796ea6671d2ecfa45260ad0b8e6df2fdb08d6bd
SnaveSutit <SnaveSutit@gmail.com>
2026-04-17 15:57:18 -0400

    🩹 Blueprint version 1.10.0
    Just fixes the animation keyframe changes in BB 5.0

--- src/formats/blueprint/versions/1.10.0.ts
+++ src/formats/blueprint/versions/1.10.0.ts
@@ -4,7 +4,33 @@ export default function upgrade(model: any): IBlueprintFormatJSON {
 	console.log('Processing model format 1.10.0', JSON.parse(JSON.stringify(model)))
 	const fixed: IBlueprintFormatJSON = JSON.parse(JSON.stringify(model))
 
-	// TODO - Fix 5.0's inverted rotations
+	// Invert rotation xy and position x for Blockbench 5.0
+	for (const animation of fixed.animations ?? []) {
+		for (const [uuid, animator] of Object.entries<Record<string, any>>(
+			animation.animators ?? {}
+		)) {
+			console.log('Processing animator', uuid, JSON.parse(JSON.stringify(animator)))
+			for (const keyframe of animator.keyframes ?? []) {
+				console.log('Processing keyframe', JSON.parse(JSON.stringify(keyframe)))
+				if (keyframe.channel === 'rotation') {
+					for (const datapoint of keyframe.data_points ?? []) {
+						console.log('Processing datapoint', JSON.parse(JSON.stringify(datapoint)))
+						datapoint.x = invertMolang(datapoint.x)
+						datapoint.y = invertMolang(datapoint.y)
+					}
+				}
+				if (keyframe.channel === 'position') {
+					for (const datapoint of keyframe.data_points ?? []) {
+						console.log(
+							'Processing position datapoint',
+							JSON.parse(JSON.stringify(datapoint))
+						)
+						datapoint.x = invertMolang(datapoint.x)
+					}
+				}
+			}
+		}
+	}
 
 	return fixed
 }
