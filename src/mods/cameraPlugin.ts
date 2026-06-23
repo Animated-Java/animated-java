9878aa045e318814b22d574e30e3de5cf9ba8ae6
SnaveSutit <SnaveSutit@gmail.com>
2026-06-23 09:35:13 -0400

    ✨ Remove roll from Cameras in BB preview

--- src/mods/cameraPlugin.ts
+++ src/mods/cameraPlugin.ts
@@ -26,11 +26,50 @@ registerPluginPatch({
 			return originalSanitize.call(this)
 		}
 
-		return { camera, originalRename, originalSanitize }
+		// @ts-expect-error
+		const cameraAnimator = OutlinerElement.types.camera.animator as BoneAnimator
+		const originalDisplayRotation = cameraAnimator.prototype.displayRotation
+		cameraAnimator.prototype.displayRotation = function (
+			this: BoneAnimator,
+			arr?: ArrayVector3 | ArrayVector4,
+			multiplier?: number
+		) {
+			originalDisplayRotation.call(this, arr, multiplier)
+			if (activeProjectIsBlueprintFormat()) {
+				console.log('Sanitizing camera rotation for blueprint format')
+				const mesh = this.getElement().mesh as THREE.Mesh
+				// Cameras in AJ don't support roll.
+				mesh.updateMatrixWorld(true)
+				mesh.getWorldQuaternion(Reusable.quat1)
+
+				const euler = Reusable.euler1.setFromQuaternion(Reusable.quat1, 'YXZ')
+				euler.z = 0 // Remove roll
+
+				const noRollWorldQuat = Reusable.quat1.setFromEuler(euler)
+
+				if (mesh.parent) {
+					const parentWorldQuat = Reusable.quat2
+					mesh.parent.getWorldQuaternion(parentWorldQuat)
+					noRollWorldQuat.premultiply(parentWorldQuat.invert())
+				}
+
+				mesh.setRotationFromQuaternion(noRollWorldQuat)
+				mesh.updateMatrix()
+			}
+		}
+
+		return { camera, originalRename, originalSanitize, cameraAnimator, originalDisplayRotation }
 	},
 
-	revert: ({ camera, originalRename, originalSanitize }) => {
+	revert: ({
+		camera,
+		originalRename,
+		originalSanitize,
+		cameraAnimator,
+		originalDisplayRotation,
+	}) => {
 		camera.prototype.rename = originalRename
 		camera.prototype.sanitizeName = originalSanitize
+		cameraAnimator.prototype.displayRotation = originalDisplayRotation
 	},
 })
