37a6503eab60128a1d355b9eafafb5ae25cb0cb0
SnaveSutit <SnaveSutit@gmail.com>
2026-08-30 08:16:29 -0400

    ✂️ Reduce entity names to empty strings to improve performance

--- src/systems/datapackCompiler/entityNames.ts
+++ src/systems/datapackCompiler/entityNames.ts
@@ -1,24 +1,7 @@
-import { TextComponent } from 'book-and-quill'
-import { makeTagSafe } from './tags'
-
 namespace ENTITY_NAMES {
-	export const ROOT = (exportNamespace: string) =>
-		new TextComponent([
-			'',
-			{ text: makeTagSafe(exportNamespace), color: '#00aced' },
-			'.',
-			{ text: 'root', color: 'light_purple' },
-		]).toString(true)
+	export const ROOT = (exportNamespace: string) => '""'
 
-	export const NODE = (exportNamespace: string, type: string, name: string) =>
-		new TextComponent([
-			'',
-			{ text: makeTagSafe(exportNamespace), color: '#00aced' },
-			'.',
-			{ text: type, color: 'light_purple' },
-			'.',
-			{ text: name, color: 'gold' },
-		]).toString(true)
+	export const NODE = (exportNamespace: string, type: string, name: string) => '""'
 }
 
 export default ENTITY_NAMES
